fbe668767fef54e10273c65bbc982cd0e95023e1
aabsharsoftw <aabshar.softw@gmail.com>
2023-03-27 10:37:36 +0530

    pull

--- src/screen/Chat/index.js
+++ src/screen/Chat/index.js
@@ -2,44 +2,41 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-native/no-inline-styles */
 
-import React, {useState, useEffect} from 'react';
-import {BgImage} from '../../component/ImageContainer';
+import React, { useState, useEffect } from "react";
+import { BgImage } from "../../component/ImageContainer";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import Ionicons from "react-native-vector-icons/dist/Ionicons";
+import { TextInput } from "react-native-gesture-handler";
+import messaging from "@react-native-firebase/messaging";
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-import Ionicons from 'react-native-vector-icons/dist/Ionicons';
-import {TextInput} from 'react-native-gesture-handler';
-import messaging from '@react-native-firebase/messaging';
-import {getChatApi, getImamChatApi, postChatApi} from '../../services/chatApi';
-import {useAuth} from '../../hook/useAuth';
-import {timeago} from '../../util/commen';
-import _sortBy from 'lodash';
+  getChatApi,
+  getImamChatApi,
+  postChatApi,
+} from "../../services/chatApi";
+import { useAuth } from "../../hook/useAuth";
+import { timeago } from "../../util/commen";
+import _sortBy from "lodash";
 
-const ChatScreen = ({navigation, route}) => {
-  const {imamId,imamName,role} = route.params;
-  const [text, setText] = useState('');
+const ChatScreen = ({ navigation, route }) => {
+  const { imamId, imamName, role } = route.params;
+  const [text, setText] = useState("");
   const [dataChat, setDataChat] = React.useState([]);
   const [imamChat, setImamChat] = useState();
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(true);
-  const [isCheck, setIscheck] = useState('');
+  const [isCheck, setIscheck] = useState("");
 
-  const {auth, state} = useAuth();
+  const { auth, state } = useAuth();
   let listView;
   const fetchUserData = async () => {
-    const result = await AsyncStorage.getItem('user');
+    const result = await AsyncStorage.getItem("user");
   };
 
   const datClone = JSON.parse(JSON.stringify(dataChat));
   const sortedActivities = datClone.sort((a, b) => {
-   
     return new Date(b.created_at) - new Date(a.created_at);
   });
-
 
   const userChatHandler = async () => {
     const data = {
@@ -57,7 +54,7 @@ const ChatScreen = ({navigation, route}) => {
     }
   };
   useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
       // Alert.alert('Message handled in the background!',JSON.stringify(remoteMessage),);
       apisHandlers();
       userChatHandler();
@@ -86,19 +83,19 @@ const ChatScreen = ({navigation, route}) => {
   useEffect(async () => {
     await userChatHandler();
     await imamChatHandler();
-  }, [imamId, state.user,refreshing]);
+  }, [imamId, state.user, refreshing]);
 
-  useEffect(()=>{
-      if (role === 'imam') {
-        setIscheck(imamName);
-      }
-  },[]);
+  useEffect(() => {
+    if (role === "imam") {
+      setIscheck(imamName);
+    }
+  }, []);
 
   const onSendMessage = async () => {
-      var d = new Date();
+    var d = new Date();
     const h = d.getHours();
     const m = d.getMinutes();
-    const timeForm = h + ':' + m;
+    const timeForm = h + ":" + m;
     var data = {
       sender_id: state.user.id,
       reciever_id: imamId,
@@ -112,7 +109,7 @@ const ChatScreen = ({navigation, route}) => {
       setRefreshing(!refreshing);
       // await imamChatHandler()
     }
-    setText('');
+    setText("");
   };
 
   const flatList = React.useRef(null);
@@ -122,47 +119,51 @@ const ChatScreen = ({navigation, route}) => {
       <View
         style={{
           marginTop: 30,
-          flexDirection: 'row',
-          alignItems:'center',
-          alignSelf: 'flex-start',
-        }}>
+          flexDirection: "row",
+          alignItems: "center",
+          alignSelf: "flex-start",
+        }}
+      >
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Ionicons name="md-chevron-back" color={'#FFFFFF'} size={25} />
+          <Ionicons name="md-chevron-back" color={"#FFFFFF"} size={25} />
         </TouchableOpacity>
-        <Text style={{color: '#FFFFFF', fontSize: 18, marginLeft: 15}}>
-          {isCheck === imamName ? `Chat With Imam ${imamName}` : `Chat With ${imamName}` }
+        <Text style={{ color: "#FFFFFF", fontSize: 18, marginLeft: 15 }}>
+          {isCheck === imamName
+            ? `Chat With Imam ${imamName}`
+            : `Chat With ${imamName}`}
         </Text>
       </View>
 
       <View
         style={{
-          backgroundColor: 'rgba(0, 6, 16, 1)',
+          backgroundColor: "rgba(0, 6, 16, 1)",
           height: 100,
-          position: 'absolute',
+          position: "absolute",
           bottom: 0,
           left: 0,
           right: 0,
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          overflow: 'hidden',
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+          overflow: "hidden",
           zIndex: 1,
-        }}>
+        }}
+      >
         <TextInput
-          onChangeText={value => setText(value)}
+          onChangeText={(value) => setText(value)}
           value={text}
           multiline={true}
           placeholder="Message...."
-          placeholderTextColor={'#FFFFFF'}
+          placeholderTextColor={"#FFFFFF"}
           style={{
-            width: '80%',
-            backgroundColor: 'rgba(157, 157, 157, 0.2)',
+            width: "80%",
+            backgroundColor: "rgba(157, 157, 157, 0.2)",
             borderRadius: 15,
-            color: '#FFFFFF',
+            color: "#FFFFFF",
           }}
         />
         <TouchableOpacity onPress={() => onSendMessage()}>
-          <Ionicons name="send" color={'#FFFFFF'} size={30} />
+          <Ionicons name="send" color={"#FFFFFF"} size={30} />
         </TouchableOpacity>
       </View>
 
@@ -174,80 +175,97 @@ const ChatScreen = ({navigation, route}) => {
             flatList.current.scrollToEnd();
         }}
       > */}
-      <View style={{marginBottom: 160}}>
+      <View style={{ marginBottom: 160 }}>
         <FlatList
           data={dataChat}
           showsVerticalScrollIndicator={false}
-          renderItem={({item}) => {
+          renderItem={({ item }) => {
             const d = new Date(item.created_at);
             const h = d.getHours();
             const m = d.getMinutes();
             let agotime = timeago(d);
-            const timeForm = h + ':' + m;
+            const timeForm = h + ":" + m;
             return (
               <View>
                 <View
                   style={{
                     marginVertical: 15,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                  }}>
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
                   {state.user.id != item.sender_id ? (
                     <View
                       style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        width: '100%',
-                        justifyContent: 'flex-start',
-                      }}>
+                        display: "flex",
+                        flexDirection: "row",
+                        width: "100%",
+                        justifyContent: "flex-start",
+                      }}
+                    >
                       <View
                         style={{
                           backgroundColor:
                             state.user.id == item.sender_id
-                              ? '#00BF9D'
-                              : '#A758EB',
+                              ? "#00BF9D"
+                              : "#A758EB",
                           borderTopRightRadius: 10,
                           borderBottomRightRadius: 10,
                           borderBottomLeftRadius: 10,
                           padding: 8,
-                        }}>
+                        }}
+                      >
                         {text !== null && (
-                          <Text style={{color: '#FFFFFF', fontSize: 18}}>
+                          <Text style={{ color: "#FFFFFF", fontSize: 18 }}>
                             {item.text}
                           </Text>
                         )}
                       </View>
-                      <View style={{ position:'absolute', left:0, marginTop:-20}}>
-                        <Text style={{color: 'grey'}}>{agotime}</Text>
+                      <View
+                        style={{
+                          position: "absolute",
+                          left: 0,
+                          marginTop: -20,
+                        }}
+                      >
+                        <Text style={{ color: "grey" }}>{agotime}</Text>
                       </View>
                     </View>
                   ) : (
                     <View
                       style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        width: '100%',
-                        justifyContent: 'flex-end',
-                      }}>
-                      <View style={{position:'absolute', right:0, marginTop: -20}}>
-                        <Text style={{color: 'grey'}}>{agotime}</Text>
+                        display: "flex",
+                        flexDirection: "row",
+                        width: "100%",
+                        justifyContent: "flex-end",
+                      }}
+                    >
+                      <View
+                        style={{
+                          position: "absolute",
+                          right: 0,
+                          marginTop: -20,
+                        }}
+                      >
+                        <Text style={{ color: "grey" }}>{agotime}</Text>
                       </View>
 
                       <View
                         style={{
                           backgroundColor:
                             state.user.id == item.sender_id
-                              ? '#00BF9D'
-                              : '#A758EB',
+                              ? "#00BF9D"
+                              : "#A758EB",
                           borderTopStartRadius: 14,
                           borderBottomLeftRadius: 14,
                           borderBottomEndRadius: 14,
                           padding: 10,
-                        }}>
+                        }}
+                      >
                         {text !== null && (
-                          <Text style={{color: '#FFFFFF', fontSize: 17}}>
+                          <Text style={{ color: "#FFFFFF", fontSize: 17 }}>
                             {item.text}
                           </Text>
                         )}
